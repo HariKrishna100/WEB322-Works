f4f938a667b36cd999b8a445d0eb8a190b1c718a
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-02-27 13:55:09 -0500

    Person Module added PostgrelSQl

--- lab/4/server.js
+++ lab/4/server.js
@@ -1,33 +1,3 @@
-// var express = require("express");
-// var app = express();
-// var path = require("path");
-
-// var HTTP_PORT = process.env.PORT || 8080;
-
-// // call this function after the http server starts listening for requests
-// function onHttpStart() {
-//   console.log("Express http server listening on: " + HTTP_PORT);
-// }
-
-// // setup a 'route' to listen on the default url path (http://localhost)
-// app.get("/", function(req,res){
-//     res.sendFile(path.join(__dirname,"/views/home.html"));
-//     //res.send("Hello World<br /><a href='/contact'>Go to the contact page</a>");
-// });
-
-// // setup another route to listen on /about
-// app.get("/contact", function(req,res){
-//   res.sendFile(path.join(__dirname,"/views/contact.html"));
-// })
-
-// // setup another route to listen on /jobs
-// app.get("/careers", function(req,res){
-//   res.sendFile(path.join(__dirname,"/views/careers.html"));
-// })
-
-// // setup http server to listen on HTTP_PORT
-// app.listen(HTTP_PORT, onHttpStart);
-
 const Sequelize = require('sequelize');
 
 // set up sequelize to point to our postgres database
@@ -41,25 +11,53 @@ var sequelize = new Sequelize('zbjwiusf', 'zbjwiusf', 'cVxj5Ohedg2yhsXUX5BXoLvIJ
     query: { raw: true }
 });
 
-// Define a "Project" model
-var Project = sequelize.define('Project', {
-    title: Sequelize.STRING,
-    description: Sequelize.TEXT
+// Person model
+var Pesron = sequelize.define('Person', {
+    fname: Sequelize.STRING,  // first Name
+    lname: Sequelize.STRING, // Last Name
+    age: Sequelize.INTEGER,
 });
-
-// synchronize the Database with our models and automatically add the 
-// table if it does not exist
 
 sequelize.sync().then(function () {
+    // create
+    Pesron.create({
+        fname: "Mathew",
+        lname: "Ogolla",
+        age: 70
+    }).then(function(){console.log("Mathew Ogolla created")});
 
-    // create a new "Project" and add it to the database
-    Project.create({
-        title: 'Project1',
-        description: 'First Project'
-    }).then(function (project) {
-        // you can now access the newly created Project via the variable project
-        console.log("success!")
-    }).catch(function (error) {
-        console.log("something went wrong!");
+    Pesron.create({
+        fname: "Billy",
+        lname: "Williams",
+        age: 40
+    }).then(function(){console.log("Billy Williams created")});
+
+    Pesron.create({
+        fname: "Arap",
+        lname: "Moi",
+        age: 100
+    }).then(function(){console.log("Arap Moi created")});
+
+    // read
+    Pesron.findAll({
+        attribute: ['fname']
+    }).then(function(data){
+        console.log("All first names");
+        for(var i =0; i < data.lenght; i++){
+            console.log(data[i].fname);
+        }
     });
+
+    // Update
+    Pesron.update({
+        lname: "Williams"
+    }, {
+        where: {id: 2}
+    }).then(function () { console.log("successfully updated user 2"); });
+
+    // Delete
+    Pesron.destroy({
+        where: { id: 1 }
+    }). then(function() {console.log("successfully removed user 1");});
 });
+
